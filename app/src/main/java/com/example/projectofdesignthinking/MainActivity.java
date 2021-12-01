bde1336cd2a518b00cbd055497ee812c75705ff5
jungwooseong <32189141+jungwooseong@users.noreply.github.com>
2021-12-02 00:21:54 +0900

    second

--- app/src/main/java/com/example/projectofdesignthinking/MainActivity.java
+++ app/src/main/java/com/example/projectofdesignthinking/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //안녕수민아?
+        //손수민 바보
     }
 }
